91523833a57ebcfcde93ce8a7fae60d4e231d291
kaylbie <audrius0917@gmail.com>
2024-02-14 23:33:55 +0200

    created folders

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import {
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi"
+	"github.com/kaylbie/goapi/internal/handlers"
+	log "github.com/sirupsen/logrus"
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kaylbie/goapi
+
+go 1.22.0
