d5a9c2b141383c81c2ebb923b0eadc7be2449bf9
kaylbie <audrius0917@gmail.com>
2024-02-15 00:37:58 +0200

    API practice

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,23 @@
 package main
 
-import {
+import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi"
+	"github.com/go-chi/chi"
 	"github.com/kaylbie/goapi/internal/handlers"
 	log "github.com/sirupsen/logrus"
-}
+)
 
+func main() {
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GO API...")
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
+}
